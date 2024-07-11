465bf8856c5bca5bf9b6624c03e5a49bd9c210ed
Gajendra <gajendraptsingh05@gmail.com>
2024-07-11 04:45:56 -0700

    commit

--- app/src/main/java/com/info/testtuts/MainActivity.kt
+++ app/src/main/java/com/info/testtuts/MainActivity.kt
@@ -1,16 +1,20 @@
 package com.info.testtuts
 
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.navigation.NavigationView
 import com.info.testtuts.adapters.PostAdapter
 import com.info.testtuts.databinding.ActivityMainBinding
 import com.info.testtuts.viewModel.PostViewModel
@@ -31,6 +35,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setupNavigationDrawer()
 
         setupRecyclerView()
 
@@ -59,6 +65,50 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+    }
+
+    private fun setupNavigationDrawer() {
+        val toggle = ActionBarDrawerToggle(
+            this,
+            binding.drawerLayout,
+            binding.toolbar,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        binding.navigationView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.nav_item1 -> {
+                    // Handle navigation item 1 click
+                    true
+                }
+                R.id.nav_item2 -> {
+                    // Handle navigation item 2 click
+                    true
+                }
+                R.id.nav_item3 -> {
+                    // Handle navigation item 3 click
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            if (binding.drawerLayout.isDrawerOpen(binding.navigationView)) {
+                binding.drawerLayout.closeDrawer(binding.navigationView)
+            } else {
+                binding.drawerLayout.openDrawer(binding.navigationView)
+            }
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun setupRecyclerView() {
